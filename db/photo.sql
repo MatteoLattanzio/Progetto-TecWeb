5367b524ee4d303ca14f63be53a9cd739c75c24e
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-03 19:27:02 +0100

    Aggiunto create db

--- db/photo.sql
+++ db/photo.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `photo`
 --
-
+CREATE DATABASE IF NOT EXISTS `photo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `photo`;
 -- --------------------------------------------------------
 
 --
