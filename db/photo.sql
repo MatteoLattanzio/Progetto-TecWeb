b1da615be04fe5a530eb392352b759e0b9445629
ila <ilarya9531@gmail.com>
2020-03-04 17:00:13 +0100

    db aggiornato

--- db/photo.sql
+++ db/photo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mar 03, 2020 alle 17:42
+-- Creato il: Mar 04, 2020 alle 16:59
 -- Versione del server: 10.4.10-MariaDB
 -- Versione PHP: 7.3.12
 
@@ -21,8 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `photo`
 --
-CREATE DATABASE IF NOT EXISTS `photo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `photo`;
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +45,14 @@ CREATE TABLE `categorie` (
   `nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nome`) VALUES
+(1, '1'),
+(2, '2');
+
 -- --------------------------------------------------------
 
 --
@@ -54,13 +61,26 @@ CREATE TABLE `categorie` (
 
 CREATE TABLE `foto` (
   `id` int(11) NOT NULL,
+  `titolo` varchar(255) NOT NULL,
   `venditore` varchar(255) NOT NULL,
   `prezzo` double NOT NULL,
   `stato` varchar(255) NOT NULL,
   `categoria` int(11) NOT NULL,
+  `data` date NOT NULL,
   `tag1` varchar(255) NOT NULL,
   `tag2` varchar(255) NOT NULL,
   `tag3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `piaciuti`
+--
+
+CREATE TABLE `piaciuti` (
+  `foto` int(11) NOT NULL,
+  `utente` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -93,6 +113,14 @@ CREATE TABLE `utenti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dump dei dati per la tabella `utenti`
+--
+
+INSERT INTO `utenti` (`nome`, `cognome`, `data`, `indirizzo`, `username`, `password`, `email`, `tipo`) VALUES
+('aa', 'aa', '0000-00-00', 'aa', 'aaa', 'aaaa', 'aa@aa', 'user'),
+('user', 'user', '2020-03-10', 'user', 'user', 'user', 'user', 'user');
+
+--
 -- Indici per le tabelle scaricate
 --
 
@@ -117,6 +145,13 @@ ALTER TABLE `foto`
   ADD PRIMARY KEY (`id`),
   ADD KEY `venditore` (`venditore`),
   ADD KEY `categoria` (`categoria`);
+
+--
+-- Indici per le tabelle `piaciuti`
+--
+ALTER TABLE `piaciuti`
+  ADD KEY `foto` (`foto`),
+  ADD KEY `utente` (`utente`);
 
 --
 -- Indici per le tabelle `preferiti`
@@ -146,13 +181,13 @@ ALTER TABLE `carrello`
 -- AUTO_INCREMENT per la tabella `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT per la tabella `preferiti`
@@ -177,6 +212,13 @@ ALTER TABLE `carrello`
 ALTER TABLE `foto`
   ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`venditore`) REFERENCES `utenti` (`username`),
   ADD CONSTRAINT `foto_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categorie` (`id`);
+
+--
+-- Limiti per la tabella `piaciuti`
+--
+ALTER TABLE `piaciuti`
+  ADD CONSTRAINT `piaciuti_ibfk_1` FOREIGN KEY (`foto`) REFERENCES `foto` (`id`),
+  ADD CONSTRAINT `piaciuti_ibfk_2` FOREIGN KEY (`utente`) REFERENCES `utenti` (`username`);
 
 --
 -- Limiti per la tabella `preferiti`
