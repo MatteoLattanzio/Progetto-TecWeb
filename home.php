51b4410fe882f8f3bc8c3581f1c6134ab2227be5
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-04 12:24:30 +0100

    Header, footer e metatag importati

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 	require_once "php/footer.php";
 
 	$output = file_get_contents("html/home.html");
-	//$output = str_replace("<meta/>", file_get_contents("html/head.html"), $output);
+	$output = str_replace("<meta/>", file_get_contents("html/meta.html"), $output);
 	$output = str_replace("<div id=\"header\"></div>", Header::build(), $output);
 	$output = str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
 	$output = str_replace("href=\"home.php\"", "", $output);
