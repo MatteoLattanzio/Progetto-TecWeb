353b09034664bfda0e7a7ad75089ee57f0772702
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-06 17:07:34 +0100

    Fix error

--- login.php
+++ login.php
@@ -11,7 +11,8 @@
 	$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
 	$output=str_replace("<div id=\"header\"></div>", Header::build(), $output);
 	$output=str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
-	$output=str_replace("%err%",getError($errors),$output);
+	$output=str_replace("%err%",getErrorLogin($errors),$output);
+	$output=str_replace("%err2%",getErrorReg($errors),$output);
 	
 	echo $output;
 ?>
