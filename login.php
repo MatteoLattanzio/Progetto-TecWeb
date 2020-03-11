37936d29332326c85ca20534206adff138393b22
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-12 00:01:01 +0100

    fix sessioni

--- login.php
+++ login.php
@@ -2,14 +2,20 @@
 	require_once "php/header.php";
 	require_once "php/footer.php";
 	require_once "php/dbhandler.php";
-	
-	$connessione=connessione();
-	if(!isset($_SESSION))
-		session_start();
 	require_once "php/server.php";
 	require_once "php/checkLogin.php";
 	require_once "php/checkRegistrazione.php";
 	
+	$connessione=connessione();
+	if(!isset($_SESSION))
+		session_start();
+	if(isset($_SESSION["username"]) && $_SESSION["type"]=="user"){
+		header("Location: profile.php");
+		exit();
+	}else if(isset($_SESSION["username"]) && $_SESSION["type"]=="admin"){
+		header("Location: admin.php");
+		exit();
+	}
 	
 	$output=file_get_contents("html/login.html");
 	$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
