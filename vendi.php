ca16f715545e3fb9220ddb0a93e3fc8bd981f693
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-11 14:33:02 +0100

    Fix vendi

--- vendi.php
+++ vendi.php
@@ -2,13 +2,14 @@
 	require_once "php/header.php";
 	require_once "php/footer.php";
 	require_once "php/dbhandler.php";
+	require_once "php/loadImg.php";
 	
 	$connessione=connessione();
 	if(!isset($_SESSION))
 		session_start();
-	require_once "php/loadImg.php";
+	
 	$output=file_get_contents("html/vendi.html");
-		$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
+	$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
 	$output=str_replace("<div id=\"header\"></div>", Header::build(), $output);
 	$output=str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
 	echo $output;
