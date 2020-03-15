84c9db67c3f40373d7b9c3b390f4a5d5c73201ab
ila <ilarya9531@gmail.com>
2020-03-15 18:06:41 +0100

    sistemato pagine vendita

--- vendi.php
+++ vendi.php
@@ -7,11 +7,17 @@
 	$connessione=connessione();
 	if(!isset($_SESSION))
 		session_start();
-	
-	$output=file_get_contents("html/vendi.html");
-	$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
+
+	if(isset($_SESSION['username'])){
+		$output=file_get_contents("html/vendi.html");
+	}
+	else{
+		$output=file_get_contents("html/vendi_sample.html");
+
+	}
+	$output=str_replace("<meta/>", file_get_contents("html/meta.html"), $output);
 	$output=str_replace("<div id=\"header\"></div>", Header::build(), $output);
 	$output=str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
+
 	echo $output;
 ?>
-
