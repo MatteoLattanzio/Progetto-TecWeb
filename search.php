de684613a6bf94c85ccb1cc77d80da4fed88eead
Alessio Barbiero <53819186+aleBarbiero@users.noreply.github.com>
2020-03-17 21:11:14 +0100

    Ricerca

--- search.php
+++ search.php
@@ -1,34 +1,19 @@
 <?php
 	require_once "php/dbhandler.php";
+	require_once "php/header.php";
+	require_once "php/footer.php";
 	require_once "php/checkSearch.php";
-	$connessione = connessione();
-	$word=search();
-	
-	function risultatiRicerca(){
-		global $connessione;
-		global $word;
-		$output='';
-		if($word!=''){
-			$categorie = $connessione->query("SELECT nome FROM categorie WHERE nome='$word';");
-		}else if (!$categorie = $connessione->query("SELECT nome FROM categorie ORDER BY nome;")) {
-			header("Location: notfound.php");
-			exit();
-		}
-		if(mysqli_num_rows($categorie) == 0)
-				$output .= "<div class=\"noRes\"><p>Spiacente, nessun risultato!</p></div>";
-		while ($categoria = $categorie->fetch_assoc()) {
-			$nome = $categoria["nome"];
-			$output .= "<div class=\"categoria\">
-				 <p>" . $nome . "</p>
-			 </div>";
-		}
-		return $output;
-		
+	$connessione=connessione();
+	if(notFound())
+		$output=file_get_contents("html/badSearch.html");
+	else{
+		$output=file_get_contents("html/goodSearch.html");
+		$output=str_replace("<div result/>",getImgSubGallery($found),$output);
 	}
-	
-	$output = file_get_contents("html/search.html");
-	$output = str_replace("value=\"\"","value=\"".$word."\"",$output);
-	$output = str_replace("<div class=\"search_result\"/>", risultatiRicerca(), $output);
+	$output=str_replace("<div id=\"header\"></div>", Header::build(), $output);
+	$output=str_replace("<div id=\"footer\"></div>", Footer::build(), $output);
+	$output=str_replace("<meta/>",file_get_contents("html/meta.html"),$output);
+	$output=str_replace("%value%",$value,$output);
 	
 	echo $output;
 ?>
