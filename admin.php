9f8af6a7ecb3e54ca0017390e7e97eb5eff3b728
Cionsa <daimatty@gmail.com>
2020-05-11 15:48:43 +0200

    Modifica container admin

--- admin.php
+++ admin.php
@@ -34,64 +34,58 @@
 		global $connessione;
 		$list="<div id=\"inApp\">";
 		$result=$connessione->query("SELECT * FROM foto WHERE stato='in attesa' ORDER BY data");
-		if(mysqli_num_rows($result)==0)
-				$list.="<p>Non ci sono immagini da approvare.</p>";
-		else{
-			while($img=$result->fetch_assoc()){
-				$idC=$img["categoria"];
-				$find=$connessione->query("SELECT nome from categorie WHERE id='$idC'");
-				$res=$find->fetch_assoc();
-				$idC=$res["nome"];
-				$idImg=$img["id"];
-				if(file_exists("upload/".$idImg.'.png')){
-					$url="upload/".$idImg.'.png';
-				}else if(file_exists("upload/".$idImg.'.jpg')){
-						$url="upload/".$idImg.'.jpg';
-				}else if(file_exists("upload/".$idImg.'.jpeg')){
-						$url="upload/".$idImg.'.jpeg';
-				}
-				$list.="<div id=\"boxFormApp\">
-							<div id=\"imgInApp\">
-								<img src=\"".$url."\" alt=\"immagine da approvare\"/>
-							</div>
-							<div id=\"formApp\">
-								<form method=\"post\" action=\"admin.php\">
-									<div id=\"modify\">
-										<input type=\"hidden\" value=\"".$img["id"]."\" name=\"id\"/>
-										<div class=\"inputContactwide\">
-											<label for=\"titolo\">Titolo</label>
-											<input type=\"text\" name=\"titolo\" value=\"".$img["titolo"]."\" readonly=\"readonly\"/>
-										</div>
-										<div class=\"inputContactwide\">
-											<label for=\"autore\">Autore</label>
-											<input type=\"text\" name=\"autore\" value=\"".$img["venditore"]."\" readonly=\"readonly\"/>
-										</div>
-										<div class=\"inputContactwide\">
-											<label for=\"categoria\">Categoria</label>
-											<input type=\"text\" name=\"categoria\" value=\"".$idC."\" readonly=\"readonly\"/>
-										</div>
-										<div class=\"inputContactwide\">
-											<label for=\"tag1\">Tag1</label>
-											<input type=\"text\" name=\"tag1\" value=\"".$img["tag1"]."\"/>
-										</div>
-										<div class=\"inputContactwide\">
-											<label for=\"tag2\">Tag2</label>
-											<input type=\"text\" name=\"tag2\" value=\"".$img["tag2"]."\"/>
-										</div>
-										<div class=\"inputContactwide\">
-											<label for=\"tag3\">Tag3</label>
-											<input type=\"text\" name=\"tag3\" value=\"".$img["tag3"]."\"/>
-										</div>
-									</div>
-									<div class=\"adminButtons\">
-										<button class=\"submitButton\" type=\"submit\" name=\"ok\">Approva</button>
-										<button class=\"submitButton\" type=\"submit\" name=\"alter\" value=\"Modifica e approva\">Modifica e approva</button>
-										<button class=\"submitButton\" type=\"submit\" name=\"no\" id=\"del\">Elimina</button>
-									</div>
-								</form>
-							</div>
-						</div>";
+		while($img=$result->fetch_assoc()){
+			$idC=$img["categoria"];
+			$find=$connessione->query("SELECT nome from categorie WHERE id='$idC'");
+			$res=$find->fetch_assoc();
+			$idC=$res["nome"];
+			$idImg=$img["id"];
+			if(file_exists("upload/".$idImg.'.png')){
+				$url="upload/".$idImg.'.png';
+			}else if(file_exists("upload/".$idImg.'.jpg')){
+					$url="upload/".$idImg.'.jpg';
+			}else if(file_exists("upload/".$idImg.'.jpeg')){
+					$url="upload/".$idImg.'.jpeg';
 			}
+			$list.="<div id=\"boxFormApp\">
+						<div id=\"imgInApp\">
+							<img src=\"".$url."\" alt=\"immagine da approvare\"/>
+						</div>
+						<div id=\"formApp\">
+							<form method=\"post\" action=\"admin.php\">
+									<input type=\"hidden\" value=\"".$img["id"]."\" name=\"id\"/>
+									<div class=\"inputContactwide\">
+										<label for=\"titolo\">Titolo</label>
+										<input type=\"text\" name=\"titolo\" value=\"".$img["titolo"]."\" readonly=\"readonly\"/>
+									</div>
+									<div class=\"inputContactwide\">
+										<label for=\"autore\">Autore</label>
+										<input type=\"text\" name=\"autore\" value=\"".$img["venditore"]."\" readonly=\"readonly\"/>
+									</div>
+									<div class=\"inputContactwide\">
+										<label for=\"categoria\">Categoria</label>
+										<input type=\"text\" name=\"categoria\" value=\"".$idC."\" readonly=\"readonly\"/>
+									</div>
+									<div class=\"inputContactwide\">
+										<label for=\"tag1\">Tag1</label>
+										<input type=\"text\" name=\"tag1\" value=\"".$img["tag1"]."\"/>
+									</div>
+									<div class=\"inputContactwide\">
+										<label for=\"tag2\">Tag2</label>
+										<input type=\"text\" name=\"tag2\" value=\"".$img["tag2"]."\"/>
+									</div>
+									<div class=\"inputContactwide\">
+										<label for=\"tag3\">Tag3</label>
+										<input type=\"text\" name=\"tag3\" value=\"".$img["tag3"]."\"/>
+									</div>
+								<div class=\"adminButtons\">
+									<button class=\"submitButton\" type=\"submit\" name=\"ok\">Approva</button>
+									<button class=\"submitButton\" type=\"submit\" name=\"alter\" value=\"Modifica e approva\">Modifica e approva</button>
+									<button class=\"submitButton\" type=\"submit\" name=\"no\" id=\"del\">Elimina</button>
+								</div>
+							</form>
+						</div>
+					</div>";
 		}
 		$list.="</div>";
 		return $list;
@@ -99,7 +93,7 @@
 
 	function money(){
 		global $connessione;
-		$result=$connessione->query("SELECT SUM(prezzo) AS money FROM carrello INNER JOIN Foto ON carrello.foto=foto.id WHERE carrello.stato='concluso';");
+		$result=$connessione->query("SELECT SUM(prezzo) AS money FROM carrello WHERE stato='concluso';");
 		if($result){
 			$tot=$result->fetch_assoc();
 			return 5*$tot['money']/100;
