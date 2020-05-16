cb501a34a01d1d28901d692bc70279573e31d189
Cionsa <daimatty@gmail.com>
2020-05-16 12:06:06 +0200

    Aggiunta aria-label checkButton

--- php/checkButton.php
+++ php/checkButton.php
@@ -15,11 +15,11 @@ require_once "dbhandler.php";
 				$result=$connessione->query("SELECT * FROM piaciuti WHERE foto='$idImg' AND utente='$user' ;");
 				if(!$result || mysqli_num_rows($result)==0){
 					$output=	"<form id=\"buttonsContainer\">
-									<button id=\"like-button\" type=\"button\" value=\"aggiungi-like\" onclick=\"like('".$idImg."')\"><i class=\"fa fa-thumbs-o-up\"></i></button>
+									<button id=\"like-button\" type=\"button\" value=\"aggiungi-like\" onclick=\"like('".$idImg."')\" aria-label=\"Mi Piace\"><i class=\"fa fa-thumbs-o-up\"></i></button>
 								</form>";
 				}else{
 					$output=	"<form id=\"buttonsContainer\">
-									<button id=\"like-button\" type=\"button\" value=\"rimuovi-like\" onclick=\"like(".$idImg.")\"><i class=\"fa fa-thumbs-up\"></i></button>
+									<button id=\"like-button\" type=\"button\" value=\"rimuovi-like\" onclick=\"like(".$idImg.")\"><i class=\"fa fa-thumbs-up\" aria-label=\"Rimuovi Mi Piace\"></i></button>
 								</form>";
 				}
 			}
@@ -37,12 +37,12 @@ require_once "dbhandler.php";
 				$result=$connessione->query("SELECT * FROM preferiti WHERE foto='$idImg' AND utente='$user' ;");
 				if(!$result || mysqli_num_rows($result)==0){
 					$output=	"<form id=\"buttonsContainer\">
-									<button id=\"preferiti-button\" type=\"button\" value=\"aggiungi-preferiti\" onclick=\"preferiti('".$idImg."')\"><i class=\"fa fa-heart-o\"></i></button>
+									<button id=\"preferiti-button\" type=\"button\" value=\"aggiungi-preferiti\" onclick=\"preferiti('".$idImg."')\" aria-label=\"Aggiungi ai preferiti\"><i class=\"fa fa-heart-o\"></i></button>
 								</form>";
 				}
 				else{
 					$output=	"<form id=\"buttonsContainer\">
-									<button id=\"preferiti-button\" type=\"button\" value=\"rimuovi-preferiti\" onclick=\"preferiti(".$idImg.")\"><i class=\"fa fa-heart\"></i></button>
+									<button id=\"preferiti-button\" type=\"button\" value=\"rimuovi-preferiti\" onclick=\"preferiti(".$idImg.")\" aria-label=\"Rimuovi dai preferiti\"><i class=\"fa fa-heart\"></i></button>
 								</form>";
 				}
 			}
@@ -60,14 +60,14 @@ require_once "dbhandler.php";
 				$result=$connessione->query("SELECT * FROM carrello WHERE foto='$idImg' AND utente='$user';");
 				if(!$result || mysqli_num_rows($result)==0){
 					$output=	"<form id=\"buttonsContainer\">
-									<button id=\"acquisto-button\" type=\"button\" value=\"aggiungi-carrello\" onclick=\"acquista('".$idImg."')\">Acquista</button>
+									<button id=\"acquisto-button\" type=\"button\" value=\"aggiungi-carrello\" onclick=\"acquista('".$idImg."')\" aria-label=\"Aggiungi al carrello\">Acquista</button>
 								</form>";
 				}else{
 					$cat=$result->fetch_assoc();
 					$stato=$cat['stato'];
 					if($stato=="in corso"){
 						$output=	"<form id=\"buttonsContainer\">
-										<button id=\"acquisto-button\" type=\"button\" value=\"rimuovi-carrello\" onclick=\"acquista(".$idImg.")\">Rimuovi</button>
+										<button id=\"acquisto-button\" type=\"button\" value=\"rimuovi-carrello\" onclick=\"acquista(".$idImg.")\" aria-label=\"Rimuovi oggetto dal carrello\">Rimuovi</button>
 									</form>";
 					}else
 						$output= "<form id=\"buttonsContainer\">
