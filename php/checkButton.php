09db88b6500cb5d086454e2c70794a8a8c536680
Cionsa <daimatty@gmail.com>
2020-05-22 11:54:34 +0200

    buttonsContainer id -> class

--- php/checkButton.php
+++ php/checkButton.php
@@ -14,11 +14,11 @@ require_once "dbhandler.php";
 			if(($user!=$venditore)&&($tipo=="user")){
 				$result=$connessione->query("SELECT * FROM piaciuti WHERE foto='$idImg' AND utente='$user' ;");
 				if(!$result || mysqli_num_rows($result)==0){
-					$output=	"<form id=\"buttonsContainer\">
+					$output=	"<form class=\"buttonsContainer\">
 									<button id=\"like-button\" type=\"button\" value=\"aggiungi-like\" onclick=\"like('".$idImg."')\" aria-label=\"Mi Piace\"><i class=\"fa fa-thumbs-o-up\"></i></button>
 								</form>";
 				}else{
-					$output=	"<form id=\"buttonsContainer\">
+					$output=	"<form class=\"buttonsContainer\">
 									<button id=\"like-button\" type=\"button\" value=\"rimuovi-like\" onclick=\"like(".$idImg.")\" aria-label=\"Rimuovi Mi Piace\"><i class=\"fa fa-thumbs-up\"></i></button>
 								</form>";
 				}
@@ -36,12 +36,12 @@ require_once "dbhandler.php";
 			if(($user!=$venditore)&&($tipo=="user")){
 				$result=$connessione->query("SELECT * FROM preferiti WHERE foto='$idImg' AND utente='$user' ;");
 				if(!$result || mysqli_num_rows($result)==0){
-					$output=	"<form id=\"buttonsContainer\">
+					$output=	"<form class=\"buttonsContainer\">
 									<button id=\"preferiti-button\" type=\"button\" value=\"aggiungi-preferiti\" onclick=\"preferiti('".$idImg."')\" aria-label=\"Aggiungi ai preferiti\"><i class=\"fa fa-heart-o\"></i></button>
 								</form>";
 				}
 				else{
-					$output=	"<form id=\"buttonsContainer\">
+					$output=	"<form class=\"buttonsContainer\">
 									<button id=\"preferiti-button\" type=\"button\" value=\"rimuovi-preferiti\" onclick=\"preferiti(".$idImg.")\" aria-label=\"Rimuovi dai preferiti\"><i class=\"fa fa-heart\"></i></button>
 								</form>";
 				}
@@ -59,18 +59,18 @@ require_once "dbhandler.php";
 			if(($user!=$venditore)&&($tipo=="user")){
 				$result=$connessione->query("SELECT * FROM carrello WHERE foto='$idImg' AND utente='$user';");
 				if(!$result || mysqli_num_rows($result)==0){
-					$output=	"<form id=\"buttonsContainer\">
+					$output=	"<form class=\"buttonsContainer\">
 									<button id=\"acquisto-button\" type=\"button\" value=\"aggiungi-carrello\" onclick=\"acquista('".$idImg."')\" aria-label=\"Aggiungi al carrello\">Acquista</button>
 								</form>";
 				}else{
 					$cat=$result->fetch_assoc();
 					$stato=$cat['stato'];
 					if($stato=="in corso"){
-						$output=	"<form id=\"buttonsContainer\">
+						$output=	"<form class=\"buttonsContainer\">
 										<button id=\"acquisto-button\" type=\"button\" value=\"rimuovi-carrello\" onclick=\"acquista(".$idImg.")\" aria-label=\"Rimuovi oggetto dal carrello\">Rimuovi</button>
 									</form>";
 					}else
-						$output= "<form id=\"buttonsContainer\">
+						$output= "<form class=\"buttonsContainer\">
 										<button id=\"acquisto-button\" type=\"button\" disabled>Acquistato</button>
 								</form>";
 					}
